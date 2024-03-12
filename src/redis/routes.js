5e839326643c73d82defc815910daaa370fc4667
jamilharun <jamilharunl45@gmail.com>
2024-03-13 02:33:59 +0800

    params debug01

--- src/redis/routes.js
+++ src/redis/routes.js
@@ -56,11 +56,11 @@ router.get('/shop/', async (req, res) => {
 //id == shop owner id
 // use cases when the shop owner is checkedout his shop
 router.get('/shop/:id', async (req, res) => {
-  const { _id } = req.params.id;
-  console.log('id: ', _id);;
+  const { id } = req.params.id;
+  console.log('id: ', id);;
   try {
     //fetching sorted sets from redis
-    const shop = await cache.cacheGetYourShop(_id);
+    const shop = await cache.cacheGetYourShop(id);
 
     if (!shop) {
       const data = await cache.sanityFetch(groq.qfs1df(_id));
