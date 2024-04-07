d04cc6327825a584c10088454f73e0bb71908751
jamilharun <jamilharunl45@gmail.com>
2024-04-08 01:19:09 +0800

    hoping its final

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors()); 
+app.use(cors({origin: '*'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
